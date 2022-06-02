37099a22f7bde5c74d10d670f66fc4e3a6e2fca7
Sheila20 <shilanyambura47@gmail.com>
2022-06-02 05:49:12 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,36 @@
 #!/bin/bash
-puts("/"Programming is like building a multilingual puzzle);
+#include <stdio.h>
+
+#include <string.h>
+
+
+
+/**
+ *
+ * *main - Main function of my project
+ *
+ * *
+ *
+ * *
+ *
+ * *Return: return 0 everytime
+ *
+ * */
+
+int main(void)
+
+{
+
+
+
+		char displayed[52];
+
+
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+
+				puts(displayed);
+
+					return (0);
+
+}

0f53ded8d1b511410c7c0bbb35310b41f6edfc60
Sheila20 <shilanyambura47@gmail.com>
2022-06-02 06:32:42 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
-
+#include <studio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ * Return:0 if success.
+ */
+int main(void)
+{
+	puts("/Programming is like building a multilingual puzzle");
 	return (0);
+}

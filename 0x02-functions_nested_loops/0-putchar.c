10e09e270ddf1eb12a42d17b4a3eda98bfe96715
Sheila20 <shilanyambura47@gmail.com>
2022-06-07 04:32:51 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,13 @@
 
 int main(void)
 {
-char *c = "_putchar";
-unsigned long i;
-for (i = 0; i <= sizeof(c); i++)
-_putchar(c[i]);
-_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
